3c519f6cbaca4bdf418deabd5d2d6386a8c8ee9c
miyoko <miyoko@maio.me>
2011-01-15 16:39:36 -0600

    making sure everything is up to date

--- include/config.php
+++ include/config.php
@@ -14,6 +14,17 @@ if (mysqli_connect_errno()) {
 	die("error: mysqli(): " . mysql_connect_error());
 }
 
+// this is for restricting user permissions
+// format is:
+//	  $restrict_users = array('username');
+// you can put more than one or two users, and the array can look like
+// this:
+//	  $restrict_users = array('username', 'other_username', 'next_user');
+// make sure that the username is in single quotes (') and
+// each separate element is comma-separated
+
+$restrict_users = array('test-session');
+
 // this next setting is for GLOBAL user restriction.
 // i don't see why you would EVER want to enable this, it COMPLETELY
 // disables access for users in the above array, all they can see
